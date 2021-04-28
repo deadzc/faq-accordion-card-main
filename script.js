2587cbd8eef775406408e651b86830fda8ee9c4a
Guilherme Meira <guibartmeira@gmail.com>
2021-04-27 21:39:43 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,71 @@
+var q1 = document.querySelector("#q1 span");
+
+q1.addEventListener('click',function(){
+    var a1 = document.querySelector("#a1");
+    
+    if (a1.style.display == "none"){
+        a1.style.display = "block";
+        q1.style.fontWeight= "bold";
+    }else{
+        a1.style.display = "none";
+        q1.style.fontWeight= "normal";
+    }
+});
+
+var q2 = document.querySelector("#q2 span");
+
+q2.addEventListener('click',function(){
+    var a2 = document.querySelector("#a2");
+    
+    if (a2.style.display == "none"){
+        a2.style.display = "block";
+        q2.style.fontWeight= "bold";
+    }else{
+        a2.style.display = "none";
+        q2.style.fontWeight= "normal";
+    }
+});
+
+var q3 = document.querySelector("#q3 span");
+
+q3.addEventListener('click',function(){
+    var a3 = document.querySelector("#a3");
+    
+    if (a3.style.display == "none"){
+        a3.style.display = "block";
+        q3.style.fontWeight= "bold";
+    }else{
+        a3.style.display = "none";
+        q3.style.fontWeight= "normal";
+    }
+});
+
+var q4 = document.querySelector("#q4 span");
+
+q4.addEventListener('click',function(){
+    var a4 = document.querySelector("#a4");
+    
+    if (a4.style.display == "none"){
+        a4.style.display = "block";
+        q4.style.fontWeight= "bold";
+    }else{
+        a4.style.display = "none";
+        q4.style.fontWeight= "normal";
+    }
+});
+
+var q5 = document.querySelector("#q5 span");
+
+q5.addEventListener('click',function(){
+    var a5 = document.querySelector("#a5");
+    
+    if (a5.style.display == "none"){
+        a5.style.display = "block";
+        q5.style.fontWeight= "bold";
+    }else{
+        a5.style.display = "none";
+        q5.style.fontWeight= "normal";
+    }
+});
+
+
